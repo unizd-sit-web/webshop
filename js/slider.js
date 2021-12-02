754db668062c917eff590aaef23804928033ae4e
Ivan1000RR <ividovic110@gmail.com>
2021-12-02 15:38:55 +0100

    dodane kategorije proizvoda te latest product dio

--- js/slider.js
+++ js/slider.js
@@ -11,8 +11,7 @@ const slider2 = document.getElementById("glide_2");
 =============
 Hero
 =============
-*/
-
+ */
 if (slider1) {
     new Glide(slider1, {
       type: "carousel",
@@ -25,4 +24,56 @@ if (slider1) {
     }).mount();
   }
 
-AOS.init();
+  /*
+=============
+Latest Products
+=============
+ */
+
+if (slider2) {
+  new Glide("#glide_2", {
+    type: "carousel",
+    startAt: 0,
+    perView: 4,
+    rewin: false,
+    animationDuration: 800,
+    animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+    breakpoints: {
+      1200: {
+        perView: 3,
+      },
+      768: {
+        perView: 2,
+      },
+    },
+  }).mount();
+}
+
+/*
+=============
+Related Products
+=============
+ */
+
+if (slider3) {
+  new Glide("#glide_3", {
+    type: "carousel",
+    startAt: 0,
+    perView: 4,
+    rewin: false,
+    animationDuration: 800,
+    animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+    breakpoints: {
+      1200: {
+        perView: 3,
+      },
+      768: {
+        perView: 2,
+      },
+    },
+  }).mount();
+}
+
+
+
+  AOS.init();
