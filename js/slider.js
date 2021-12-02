e8b827de62a7a3b70f5b3d39f300ee52dbeffd72
Ivan1000RR <74723310+Ivan1000RR@users.noreply.github.com>
2021-12-02 20:34:20 +0100

    Dodan info dio i newsletter
    Isprabljene greške pri merge prethodna dva commita

--- js/slider.js
+++ js/slider.js
@@ -6,6 +6,9 @@ Glide Js Carousel
 
 const slider1 = document.getElementById("glide_1");
 const slider2 = document.getElementById("glide_2");
+const slider3 = document.getElementById("glide_3");
+const slider4 = document.getElementById("glide_4");
+const slider5 = document.getElementById("glide_5");
 
 /*
 =============
@@ -13,18 +16,18 @@ Hero
 =============
  */
 if (slider1) {
-    new Glide(slider1, {
-      type: "carousel",
-      startAt: 0,
-      autoplay: 3000,
-      hoverpause: true,
-      perView: 1,
-      animationDuration: 800,
-      animationTimingFunc: "linear",
-    }).mount();
-  }
+  new Glide(slider1, {
+    type: "carousel",
+    startAt: 0,
+    autoplay: 3000,
+    hoverpause: true,
+    perView: 1,
+    animationDuration: 800,
+    animationTimingFunc: "linear",
+  }).mount();
+}
 
-  /*
+/*
 =============
 Latest Products
 =============
@@ -76,4 +79,4 @@ if (slider3) {
 
 
 
-  AOS.init();
+AOS.init();
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Projekt 1 - Ivan Vidović i Lovre Nenadić
+
+- Prvi projekt za kolegiji Programiranje za web. 
+- WebShop s najboljim izborom mobitela i mobilne tehnologijes
+s
